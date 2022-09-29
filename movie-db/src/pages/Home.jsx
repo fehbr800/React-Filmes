039cdd83e05f7a7d72997a0e31735011a6a92476
Matheus Emanoel <matheusemanoel1407@gmail.com>
2022-09-29 15:38:23 -0300

    movieSucess

--- movie-db/src/pages/Home.jsx
+++ movie-db/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
 
-
+import './MovieGrid.css'
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -27,6 +27,7 @@ const Home = () => {
     <div className="container">
       <h2 className="title">Melhores filmes:</h2>
       <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
         {topMovies.length > 0 &&
           topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
